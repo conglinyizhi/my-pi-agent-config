02b1d8277404c9319f6837ff82870b4f1b3d2dc5
conglinyizhi <conglinyizhi@qq.com>
2026-09-26 13:48:57 +0800

    feat(photo): 内嵌管理页

--- photo/page.go
+++ photo/page.go
@@ -7,3 +7,13 @@ import _ "embed"
 //
 //go:embed page.html
 var pageHTML []byte
+
+// 管理页分开放：上传页是给手机的，管理页只给主机本机，两边的读者与风险不一样，
+// 同一个文件里做二选一反而看不清哪边改了什么。
+//
+//go:embed manage.html
+var manageHTML []byte
+
+// manageArchiveSlot 是 manage.html 里归档目录路径的占位符。路径要 Serving 时
+// 才能知道（它跟着 -state 走），而又不想为一行字引 html/template 或者多开一个接口。
+var manageArchiveSlot = []byte("{{ARCHIVE}}")
